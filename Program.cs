18890bb10a6f8b4c8a177b93a44c2457d29de964
SantiQuiroga <quiroga.default.santiago@gmail.com>
2024-05-07 22:22:32 -0400

    fix user assosiation, using dictionaries

--- Program.cs
+++ Program.cs
@@ -30,18 +30,63 @@ public static class Program
     Console.WriteLine("Coordinates: ");
     Console.WriteLine(coords.ToString());
 
-    UserManager userManager = new UserManager();
+    Dictionary<string, object> user1 = new Dictionary<string, object>
+    {
+      { "Id", 1 },
+      { "Name", "John" },
+      { "LastName", "Doe" },
+      { "Birthday", new DateTime(1980, 1, 1, 0, 0, 0, DateTimeKind.Utc) },
+      { "Address", "123 Main St" }
+    };
+    
+    Dictionary<string, object> account1 = new Dictionary<string, object>
+    {
+        { "Id", 1 },
+        { "Amount", 1000m },
+        { "Status", "Active" },
+        { "Currency", "USD" }
+    };
 
-    User user1 = new User(1, "John", "Doe", new DateTime(1980, 1, 1, 0, 0, 0, DateTimeKind.Utc), "123 Main St");
-    userManager.AddUser("1", user1);
-    Account account1 = new Account(1, 1000m, "Active", "USD");
-    Account account2 = new Account(2, 3000m, "Active", "USD");
-    userManager.AddAccountToUser("1", account1);
-    userManager.AddAccountToUser("1", account2);
-    List<Account> user1Accounts = userManager.GetUserAccounts("1");
+    Dictionary<string, object> account2 = new Dictionary<string, object>
+    {
+        { "Id", 2 },
+        { "Amount", 2000m },
+        { "Status", "Active" },
+        { "Currency", "EUR" }
+    };
+
+    Dictionary<Dictionary<string, object>, List<Dictionary<string, object>>> userAccounts = 
+        new Dictionary<Dictionary<string, object>, List<Dictionary<string, object>>>
+    {
+        { user1, new List<Dictionary<string, object>> { account1, account2 } }
+    };
     
     Console.WriteLine(" ");
-    Console.WriteLine("User and account: ");
-    Console.WriteLine($"User {user1.Name} has {user1Accounts.Count} account(s).");
+    Console.WriteLine("User accounts:");
+
+    foreach (var userAccount in userAccounts)
+    {
+
+        Console.WriteLine("User:");
+        Console.WriteLine(" ");
+
+        foreach (var userDetail in userAccount.Key)
+        {
+            Console.WriteLine($"{userDetail.Key}: {userDetail.Value}");
+        }
+
+        Console.WriteLine(" ");
+        Console.WriteLine("Accounts:");
+        Console.WriteLine(" ");
+
+        foreach (var account in userAccount.Value)
+        {
+            foreach (var accountDetail in account)
+            {
+                Console.WriteLine($"{accountDetail.Key}: {accountDetail.Value}");
+            }
+            Console.WriteLine(" ");
+        }
+    }
   }
 }
